feaf471bcd3ceb0e28f6b35f7c4c9fcdc779bc33
ashleybrgr <22ashley62@gmail.com>
2017-04-27 17:00:15 -0400

    alamofire request

--- shopify-ios/shopify-ios/ViewController.swift
+++ shopify-ios/shopify-ios/ViewController.swift
@@ -7,24 +7,56 @@
 //
 
 import UIKit
+import Alamofire
+import SwiftyJSON
 
 class ViewController: UIViewController {
 
-
+  
   @IBOutlet weak var topView: UIView!
   @IBOutlet weak var topSpacer: UIView!
-  
   @IBOutlet weak var bottomSpacer: UIView!
+  
+  let URL = "https://shopicruit.myshopify.com/admin/"
+  let TOKEN = "c32313df0d0ef512ca64d5b336a0d7c6"
   
   override func viewDidLoad() {
     super.viewDidLoad()
+
   }
   
-  override func viewDidAppear(_ animated: Bool) {
+  override func viewWillAppear(_ animated: Bool) {
     stylize()
+    getOrders { (response) in
+      guard (!response["errors"].exists()) else {
+        print(response["errors"])
+        return
+      }
+      
+      
+      
+    }
+    
     
   }
 
+  func getOrders(_ cb: @escaping (_ response: JSON) -> ()){
+    Alamofire.request(URL+"orders.json", method: .get, parameters: ["page":"1", "access_token":TOKEN], encoding: URLEncoding.default, headers: nil).responseJSON { (response) in
+
+      switch(response.result) {
+      case .success(let data):
+        cb(JSON(data))
+        break
+        
+      case .failure(_):
+        cb(JSON(["errors": ["message":"Timeout."]]))
+        break
+        
+      }
+    }
+    
+  }
+  
   func stylize() {
     topView.layer.cornerRadius = topView.frame.width / 2
     topView.clipsToBounds = true
